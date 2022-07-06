4d5209a90b83443dfef078983b4725d51b8e8ef0
BuildTools <unconfigured@null.spigotmc.org>
2022-07-06 03:06:18 +0200

    Started Messenger for Windows

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        JFrame jFrame = new JFrame("Messenger by Zerakles");
+        jFrame.setSize(600,600);
+    }
+}
